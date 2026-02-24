c772e7ba46b0cc3a77adccd475e993501638a99f
Boompozhilan B M <boompozhilan341@gmail.com>
2026-02-24 20:33:05 +0530

    Update database.py

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Replace with your actual MySQL credentials
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:password@localhost/student_db"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:Tamizh%400911@localhost:3306/student_db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
